cea71c22eb0f98159194ba49645e786c88d515ae
MiguelViHe <miguelvidalhernando@gmail.com>
2024-09-24 18:10:48 +0200

    PHILO - creación de primeras estructuras y retocado para hacer las primeras purebas con los dos tenedores(2 mutex). Aún sin hacer la routine de prueba

--- philosophers/philo/includes/philo.h
+++ philosophers/philo/includes/philo.h
@@ -6,11 +6,29 @@
 /*   By: mvidal-h <mvidal-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 11:00:17 by mvidal-h          #+#    #+#             */
-/*   Updated: 2024/09/20 11:34:33 by mvidal-h         ###   ########.fr       */
+/*   Updated: 2024/09/24 15:44:50 by mvidal-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+//libft
+# include "libft/libft.h"
+
+//malloc, free, exit, rand
+# include <stdlib.h>
+
+//O_RDONLY
+# include <fcntl.h>
+
+# include <pthread.h>
+
+	typedef struct s_philo_data
+	{
+		int				philo_id;
+		pthread_mutex_t	*fork_l;
+		pthread_mutex_t	*fork_r;
+	} t_philo_data;
 
 #endif
--- philosophers/philo/src/philo.c
+++ philosophers/philo/src/philo.c
@@ -6,25 +6,63 @@
 /*   By: mvidal-h <mvidal-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 17:11:56 by mvidal-h          #+#    #+#             */
-/*   Updated: 2024/09/20 18:09:59 by mvidal-h         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:08:18 by mvidal-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-# include <fcntl.h>  //O_RDONLY
-#include <pthread.h>
+#include "../includes/philo.h"
 #include "../includes/functions.h"
 
-typedef struct s_thread_data{
-    pthread_mutex_t	*mutex;
-    int				thread_id;
-} t_thread_data;
+t_philo_data *init_philo(pthread_mutex_t *mtx_forks, int philo, int num_philos)
+{
+	t_philo_data	*philo_data;
+
+	philo_data = malloc(sizeof(t_philo_data));
+	philo_data->philo_id = philo;
+	philo_data->fork_l = &mtx_forks[philo];
+	philo_data->fork_r = &mtx_forks[(philo + 1) % num_philos];
+	return (philo_data);
+}
+
+pthread_mutex_t	*initialize_mutex_forks(int num_forks)
+{
+	int	i;
+	pthread_mutex_t	*mutex_forks;
+	
+	i = 0;
+	mutex_forks = malloc(num_forks * sizeof(pthread_mutex_t));
+	while (i < num_forks)
+		pthread_mutex_init(&mutex_forks[i], NULL);
+	return (mutex_forks);
+}
+
+void	destroy_mutex_forks(pthread_mutex_t	*mutex_forks, int num_forks)
+{
+	int	i;
+	
+	i = 0;
+	while (i < num_forks)
+		pthread_mutex_destroy(&mutex_forks[i]);
+	free(mutex_forks);
+}
+
+void	join_philos(pthread_t *philos, int num_philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_philos)
+	{
+		pthread_join(philos[i], NULL);
+		printf("join %d\n", i);
+		i++;
+	}
+	free(philos);
+}
 
 void	*thread_routine2(void *arg)
 {
-	t_thread_data	*thread_data;
+	t_philo_data	*thread_data;
 
 	thread_data = (t_thread_data *)arg;
 	printf("puerta esperando: %d\n", thread_data->thread_id);
@@ -39,43 +77,29 @@ void	*thread_routine2(void *arg)
 
 int	main(int argc, char *argv[])
 {
-	pthread_t		*forks;
-	int				num_forks;
-	t_thread_data	*thread_data;
-	pthread_mutex_t	mutex;
+	pthread_t		*philos; //liberado!
+	pthread_mutex_t	*mutex_forks; //liberado!
+	int				num_philos;
+	t_philo_data	*philo_data; // liberarlo en cada thread
 	int				i;
 	
 	if (argc != 5 && argc != 6)
-	{
-		printf("Bad arguments, Try:\n");
-		printf("./philo num_of_philosophers time_to_die time_to_eat ");
-		printf("time_to_sleep [number_of_times_each_philosopher_must_eat]");
-	}
+		printf("Bad num of arguments. Try again\n");
 	else
 	{
 		i = 0;
-		num_forks = ft_atoi(argv[1]);
-		forks = malloc(num_forks * sizeof(pthread_t));
-		pthread_mutex_init(&mutex, NULL);
-		while (i < num_forks)
+		num_philos = ft_atoi(argv[1]);
+		philos = malloc(num_philos * sizeof(pthread_t));
+		mutex_forks = initialize_mutex_forks(num_philos);
+		while (i < num_philos)
 		{
-			printf("entro aqui\n");
-			thread_data = malloc(sizeof(t_thread_data));
-			thread_data->thread_id = i;
-			thread_data->mutex = &mutex;
-			if (pthread_create(&forks[i], NULL, thread_routine2, thread_data) != 0)
+			philo_data = initialize_philo(mutex_forks, i + 1);
+			if (pthread_create(&philos[i], NULL, thread_routine2, philo_data) != 0)
 				return (-1);
 			i++;
 		}
-		i = 0;
-		while (i < num_forks)
-		{
-			pthread_join(forks[i], NULL);
-			printf("join %d\n", i);
-			i++;
-		}
-		free(forks);
-		pthread_mutex_destroy(&mutex);
+		join_philos(philos, num_philos); //aqui se libera philos
+		destroy_mutex_forks(mutex_forks, num_philos); //aqui se libera mutex_forks
 	}
 	return (0);
 }
