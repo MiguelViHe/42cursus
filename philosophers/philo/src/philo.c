4e37fa41d58236cd31abfa21ba92454a6dc522dd
MiguelViHe <miguelvidalhernando@gmail.com>
2024-10-04 14:48:03 +0200

    PHILO - mensajes modificados para que sean como dice el subject. Todavia no se paran los philos cuando uno muere. Tampoco se escribe el mensaje en los 10 ms siguientes a su muerte.

--- philosophers/philo/src/philo.c
+++ philosophers/philo/src/philo.c
@@ -6,11 +6,17 @@
 /*   By: mvidal-h <mvidal-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 17:11:56 by mvidal-h          #+#    #+#             */
-/*   Updated: 2024/10/03 18:36:24 by mvidal-h         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:45:06 by mvidal-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+// void	set_tm_last_eat(t_philo_dt *philo_data)
+// {
+// 	if (philo_data->is_alive)
+// 		philo_data->tm_last_eat = abs_time_ms();
+// }
 
 void	check_philo_alive(t_philo_dt *philo_data)
 {
@@ -51,36 +57,72 @@ void	*th_routine(void *arg)
 
 	philo_data = (t_philo_dt *)arg;
 	if (philo_data->philo_id % 2 == 0)
-		usleep(50 * 1000);
+		usleep((philo_data->table->tm_eat / 2) * 1000);
 	while (philo_data->is_alive && philo_data->tms_ph_ate != philo_data->table->n_tms_eat)
 	{
 		pthread_mutex_lock(philo_data->fork_l);
-		printf("%ld - Soy %d, mi tenedor left(%d) cogido\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->philo_id);
+		printf("%ld - %d has taken a fork(%d)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->philo_id);
 		pthread_mutex_lock(philo_data->fork_r);
-		printf("%ld - Soy %d, tenedor right(%d) cogido\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, ((philo_data->philo_id) % philo_data->table->num_philos) + 1);
+		printf("%ld - %d has taken a fork(%d)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, ((philo_data->philo_id) % philo_data->table->num_philos) + 1);
 		check_philo_alive(philo_data);
 		if (philo_data->is_alive)
 		{
-			printf("%ld - %d: empiezo a comer(%d ms)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_eat);
+			printf("%ld - %d is eating\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id);
 			usleep(philo_data->table->tm_eat * 1000);
 			if (philo_data->table->n_tms_eat >= 0)
 				philo_data->tms_ph_ate++;
-			printf("%ld - %d: termino comer (%d ms) (comi %d veces)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_eat, philo_data->tms_ph_ate);
 		}
 		pthread_mutex_unlock(philo_data->fork_r);
 		pthread_mutex_unlock(philo_data->fork_l);
 		if (philo_data->is_alive && philo_data->tms_ph_ate != philo_data->table->n_tms_eat)
 		{
-			printf("%ld - %d, empiezo a dormir (%d ms)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_sleep);
+			printf("%ld - %d  is sleeping\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id);
 			usleep(philo_data->table->tm_sleep * 1000);
-			printf("%ld - %d, me despierto (%d ms)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_sleep);
+			printf("%ld - %d,  is thinking\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id);
 		}
 	}
 	if (!philo_data->is_alive)
-		printf("%ld - Soy %d y he muerto\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id);
+		printf("%ld - %d died\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id);
 	free(philo_data);
 	return (NULL);
 }
+
+// void	*th_routine(void *arg)
+// {
+// 	t_philo_dt		*philo_data;
+
+// 	philo_data = (t_philo_dt *)arg;
+// 	if (philo_data->philo_id % 2 == 0)
+// 		usleep(50 * 1000);
+// 	while (philo_data->is_alive && philo_data->tms_ph_ate != philo_data->table->n_tms_eat)
+// 	{
+// 		pthread_mutex_lock(philo_data->fork_l);
+// 		printf("%ld - Soy %d, mi tenedor left(%d) cogido\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->philo_id);
+// 		pthread_mutex_lock(philo_data->fork_r);
+// 		printf("%ld - Soy %d, tenedor right(%d) cogido\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, ((philo_data->philo_id) % philo_data->table->num_philos) + 1);
+// 		check_philo_alive(philo_data);
+// 		if (philo_data->is_alive)
+// 		{
+// 			printf("%ld - %d: empiezo a comer(%d ms)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_eat);
+// 			usleep(philo_data->table->tm_eat * 1000);
+// 			if (philo_data->table->n_tms_eat >= 0)
+// 				philo_data->tms_ph_ate++;
+// 			printf("%ld - %d: termino comer (%d ms) (comi %d veces)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_eat, philo_data->tms_ph_ate);
+// 		}
+// 		pthread_mutex_unlock(philo_data->fork_r);
+// 		pthread_mutex_unlock(philo_data->fork_l);
+// 		if (philo_data->is_alive && philo_data->tms_ph_ate != philo_data->table->n_tms_eat)
+// 		{
+// 			printf("%ld - %d, empiezo a dormir (%d ms)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_sleep);
+// 			usleep(philo_data->table->tm_sleep * 1000);
+// 			printf("%ld - %d, me despierto (%d ms)\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id, philo_data->table->tm_sleep);
+// 		}
+// 	}
+// 	if (!philo_data->is_alive)
+// 		printf("%ld - Soy %d y he muerto\n", rel_time_ms(philo_data->table->tm_sim_start), philo_data->philo_id);
+// 	free(philo_data);
+// 	return (NULL);
+// }
 
 int	launch_simulation(pthread_t *phi, t_philo_dt **phi_dt, t_table_dt *table)
 {
